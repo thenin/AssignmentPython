a9469f36ab733ea7c0e50ce526c6e90bfa9cd656
kguryanov <kguryanov@gmail.com>
2016-02-12 12:43:09 +0200

    corrected test scenarios

--- test/TestNEgative.py
+++ test/TestNEgative.py
@@ -7,10 +7,10 @@ from verifier import verifiers
 
 class TestCaseNegative(unittest.TestCase):
     def test_simple_positive_comparison(self):
-        verifiers.Verifiers.verify_equals(1, 10, 'Comparison failed')
+        verifiers.Verifiers.verify_equals(1, simplesubject.SimpleSubject.do_sum(1, 2, 3, 4), 'Comparison failed')
 
     def test_simple_negative_comparison(self):
-        verifiers.Verifiers.verify_not_equals(1, 1, 'Comparison failed')
+        verifiers.Verifiers.verify_not_equals(10, simplesubject.SimpleSubject.do_sum(1, 2, 3, 4), 'Comparison failed')
 
     def test_verify_type(self):
         verifiers.Verifiers.verify_type(1000, str, "Incorrect type")
--- test/TestPositive.py
+++ test/TestPositive.py
@@ -7,10 +7,10 @@ from verifier import verifiers
 
 class TestCasePositive(unittest.TestCase):
     def test_simple_positive_comparison(self):
-        verifiers.Verifiers.verify_equals(1, 1, 'Comparison failed')
+        verifiers.Verifiers.verify_equals(10, simplesubject.SimpleSubject.do_sum(1, 2, 3, 4), 'Comparison failed')
 
     def test_simple_negative_comparison(self):
-        verifiers.Verifiers.verify_not_equals(1, 0, 'Comparison failed')
+        verifiers.Verifiers.verify_not_equals(1, simplesubject.SimpleSubject.do_sum(1, 2, 3, 4), 'Comparison failed')
 
     def test_verify_type(self):
         verifiers.Verifiers.verify_type("Simple string", str, "Incorrect type")
